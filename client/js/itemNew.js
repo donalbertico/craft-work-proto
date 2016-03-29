b708d444387830bc68050238c46891fda91550d7
donalbertico <donalbertico>
2016-03-29 11:36:00 -0500

    getting crud actions ready

--- client/js/itemNew.js
+++ client/js/itemNew.js
@@ -1,14 +1,37 @@
 AutoForm.hooks({
   'items-new-form': {
     onSuccess: function (operation, result, template) {
-      toast('Item created successfully!', 4000);
+       Materialize.toast('Item ingresado!', 4000) ;
     }
   }
 });
 Template.itemNew.rendered = function() {     
+    if(FlowRouter.current().route.name==='new item'){
+    	$('#newTab').addClass('active');
+    }
     $('ul.tabs').tabs(); 
-      Meteor.subscribe('items');
- var colections = items.find();
- console.log(colections);
-  }
+    $('.collapsible').collapsible();
+}
+Template.itemNew.events({
+	'click .tabs li' : function(e){
+		if(e.target.id==='newTab'){
+			window.history.pushState('','','/items/new');
+		}else{
+			window.history.pushState('','','/items');
+		}
+	},
+	'click .collapsible-header ' : function(e){
+		console.log(e);
+		$(e.currentTarget.children[1]).toggle();
+	},
+	'click .waves-effect.waves-light.delete.btn' : function(e){
+		Meteor.call('deleteItem',this._id);
+        Materialize.toast('Item borrado!', 4000) ;
+	}
+});
+Template.itemNew.helpers({
+	items(){
+		return items.find({});
+	}
+});
 
