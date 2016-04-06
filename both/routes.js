fbc336131feae560a7655a362823586e14075946
donalbertico <donalbertico>
2016-04-05 19:03:34 -0500

    adding loggin

--- both/routes.js
+++ both/routes.js
@@ -1,3 +1,11 @@
+var isLogged = function(template){
+	console.log(Meteor.userId());
+	if(Meteor.userId()=== null){
+		BlazeLayout.render('mainLayout',{main : 'loginForm'});
+	}else{
+		BlazeLayout.render('mainLayout',{main : template});
+	}
+};
 FlowRouter.route('/',{
 	name: 'home',
 	action(){
@@ -7,12 +15,18 @@ FlowRouter.route('/',{
 FlowRouter.route('/items',{
 	name: 'items',
 	action(){
-		BlazeLayout.render('mainLayout',{main : 'itemNew'});
+		isLogged('itemNew')
 	},
 });
 FlowRouter.route('/items/new',{
 	name: 'new item',
 	action(){
-		BlazeLayout.render('mainLayout',{main : 'itemNew'});
+		isLogged('itemNew')
+	},
+});
+FlowRouter.route('/login',{
+	name: 'log in',
+	action(){
+		BlazeLayout.render('mainLayout',{main : 'loginForm'});
 	},
 });
